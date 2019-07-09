cc4475ceab033c321395313fb6bc768fd04b0a9e
Ash <ashley.cruz.crespo@gmail.com>
2019-07-09 14:56:25 -0400

    reviewjs

---
+++ src/components/Content/Review.js
@@ -0,0 +1,12 @@
+render() {
+    return (
+      <div className="review-card"></div>
+<br />
+          <button className="add-review-btn">Add Review</button>
+          <button className="edit-review-btn">Edit Review</button>
+          <br />
+          <button className="save-btn">Save Review</button>
+          <button className="delete-review-btn">Delete Review</button>
+ <br />
+    );
+};
